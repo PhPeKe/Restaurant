39d2968e346a490ecd5b43e285d954348324f347
PhPeKe <p.kerstenlive.de>
2018-05-11 21:37:58 +0200

    Cant get adapter to work

--- app/src/main/java/com/example/restaurant/CategoriesActivity.java
+++ app/src/main/java/com/example/restaurant/CategoriesActivity.java
@@ -1,13 +1,40 @@
 package com.example.restaurant;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class CategoriesActivity extends Activity {
+import java.util.ArrayList;
+
+public class CategoriesActivity extends Activity implements CategoriesRequest.Callback{
+
+    private int ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CategoriesRequest request = new CategoriesRequest(this);
+        request.getCategories(this);
+    }
+
+    @Override
+    public void gotCategories(ArrayList<String> categories) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,R.id.text, categories);
+        ListView list = findViewById(R.id.list);
+        list.setAdapter(adapter);
+    }
+
+    @Override
+    public void gotCategoriesError(String message) {
+        Context context = getApplicationContext();
+        CharSequence text = message;
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
--- app/src/main/java/com/example/restaurant/CategoriesRequest.java
+++ app/src/main/java/com/example/restaurant/CategoriesRequest.java
@@ -2,40 +2,67 @@ package com.example.restaurant;
 
 import android.content.Context;
 
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.util.ArrayList;
 
-public class CategoriesRequest extends JsonObjectRequest {
+public class CategoriesRequest implements Response.Listener<JSONObject>, Response.ErrorListener{
+
+    public Context context;
+    public Callback thisActivity = null;
+
     public interface Callback {
         void gotCategories(ArrayList<String> categories);
         void gotCategoriesError(String message);
     }
 
-    Context context;
-
-    public CategoriesRequest(Context context) {
-
+    public CategoriesRequest (Context passedContext) {
+        context = passedContext;
     }
 
-    public CategoriesRequest(int method, String url, JSONObject jsonRequest, Response.Listener<JSONObject> listener, Response.ErrorListener errorListener) {
-        super(method, url, jsonRequest, listener, errorListener);
-    }
-
+    @Override
     public void onResponse(JSONObject response) {
+        System.out.println("Works!");
+        try {
+            // Get JSON-Array from response
+            JSONArray rawCategories = response.getJSONArray("categories");
+
+            // Initialize empty list for strings in response
+            ArrayList<String> categories = new ArrayList<>();
+
+            // Loop through array to extract strings
+            for (int i = 0; i < rawCategories.length(); i++) {
+                categories.add(rawCategories.getString(i));
+            }
+
+            thisActivity.gotCategories(categories);
+
+        } catch (JSONException e) {
+            // Print error message if something goes wrong while retrieving the request
+            e.printStackTrace();
+            System.out.println("Error while getting categories");
+        }
 
     }
 
+    @Override
     public void onErrorResponse(VolleyError error) {
-
+        thisActivity.gotCategoriesError(error.getMessage());
     }
 
-    public void getCategories() {
-
+    public void getCategories(Callback activity) {
+        System.out.println("Works!!!!!!!!!!!!!");
+        thisActivity = activity;
+        RequestQueue queue = Volley.newRequestQueue(context);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest("https://resto.mprog.nl/categories",null, this, this);
+        queue.add(jsonObjectRequest);
     }
 
 }
